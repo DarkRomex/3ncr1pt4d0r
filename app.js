b8c94efe0cb8a93d47063736248a08ec5f1eebbe
Geissel Rabanal <107044090+DarkRomex@users.noreply.github.com>
2024-08-27 19:54:14 -0500

    Finalizado

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
-***holas
 */
 
 function limpiar(){
